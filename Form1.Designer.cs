92b0406312d9b2c640f68e92d722b223e10eee7e
Zero Z <zero506134@hotmail.com>
2021-01-09 09:25:23 -0600

    start

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace StoneShardItemEnchantEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CharacterNames = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoadCharacter = new System.Windows.Forms.Button();
+            this.ItemListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LoadItem = new System.Windows.Forms.Button();
+            this.Enchantonitem = new System.Windows.Forms.DataGridView();
+            this.SaveFile = new System.Windows.Forms.Button();
+            this.SwapEnchant = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.EnchantList = new System.Windows.Forms.ListBox();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Enchant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EnchantAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OverAllAmount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Enchantonitem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CharacterNames
+            // 
+            this.CharacterNames.FormattingEnabled = true;
+            this.CharacterNames.Location = new System.Drawing.Point(162, 41);
+            this.CharacterNames.Name = "CharacterNames";
+            this.CharacterNames.Size = new System.Drawing.Size(121, 21);
+            this.CharacterNames.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Character";
+            // 
+            // LoadCharacter
+            // 
+            this.LoadCharacter.Location = new System.Drawing.Point(289, 41);
+            this.LoadCharacter.Name = "LoadCharacter";
+            this.LoadCharacter.Size = new System.Drawing.Size(75, 23);
+            this.LoadCharacter.TabIndex = 2;
+            this.LoadCharacter.Text = "Load";
+            this.LoadCharacter.UseVisualStyleBackColor = true;
+            this.LoadCharacter.Click += new System.EventHandler(this.LoadCharacter_Click);
+            // 
+            // ItemListBox
+            // 
+            this.ItemListBox.FormattingEnabled = true;
+            this.ItemListBox.Location = new System.Drawing.Point(36, 135);
+            this.ItemListBox.Name = "ItemListBox";
+            this.ItemListBox.Size = new System.Drawing.Size(136, 251);
+            this.ItemListBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Items";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(328, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enchant";
+            // 
+            // LoadItem
+            // 
+            this.LoadItem.Location = new System.Drawing.Point(197, 246);
+            this.LoadItem.Name = "LoadItem";
+            this.LoadItem.Size = new System.Drawing.Size(52, 23);
+            this.LoadItem.TabIndex = 7;
+            this.LoadItem.Text = ">>>";
+            this.LoadItem.UseVisualStyleBackColor = true;
+            this.LoadItem.Click += new System.EventHandler(this.LoadItem_Click);
+            // 
+            // Enchantonitem
+            // 
+            this.Enchantonitem.AllowUserToAddRows = false;
+            this.Enchantonitem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Enchantonitem.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Enchant,
+            this.EName,
+            this.EnchantAmount,
+            this.OverAllAmount});
+            this.Enchantonitem.Location = new System.Drawing.Point(255, 135);
+            this.Enchantonitem.Name = "Enchantonitem";
+            this.Enchantonitem.ReadOnly = true;
+            this.Enchantonitem.Size = new System.Drawing.Size(457, 251);
+            this.Enchantonitem.TabIndex = 9;
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.Location = new System.Drawing.Point(439, 39);
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Size = new System.Drawing.Size(75, 23);
+            this.SaveFile.TabIndex = 11;
+            this.SaveFile.Text = "Save";
+            this.SaveFile.UseVisualStyleBackColor = true;
+            this.SaveFile.Click += new System.EventHandler(this.SaveFile_Click);
+            // 
+            // SwapEnchant
+            // 
+            this.SwapEnchant.Location = new System.Drawing.Point(510, 102);
+            this.SwapEnchant.Name = "SwapEnchant";
+            this.SwapEnchant.Size = new System.Drawing.Size(75, 23);
+            this.SwapEnchant.TabIndex = 12;
+            this.SwapEnchant.Text = "Swap";
+            this.SwapEnchant.UseVisualStyleBackColor = true;
+            this.SwapEnchant.Click += new System.EventHandler(this.SwapEnchant_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(768, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "List of Know Enchnats";
+            // 
+            // EnchantList
+            // 
+            this.EnchantList.FormattingEnabled = true;
+            this.EnchantList.Location = new System.Drawing.Point(761, 135);
+            this.EnchantList.Name = "EnchantList";
+            this.EnchantList.Size = new System.Drawing.Size(120, 251);
+            this.EnchantList.TabIndex = 14;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 30;
+            // 
+            // Enchant
+            // 
+            this.Enchant.HeaderText = "Enchant";
+            this.Enchant.Name = "Enchant";
+            this.Enchant.ReadOnly = true;
+            // 
+            // EName
+            // 
+            this.EName.HeaderText = "IGN";
+            this.EName.Name = "EName";
+            this.EName.ReadOnly = true;
+            // 
+            // EnchantAmount
+            // 
+            this.EnchantAmount.HeaderText = "Amount %";
+            this.EnchantAmount.Name = "EnchantAmount";
+            this.EnchantAmount.ReadOnly = true;
+            this.EnchantAmount.Width = 60;
+            // 
+            // OverAllAmount
+            // 
+            this.OverAllAmount.HeaderText = "OverallAmount";
+            this.OverAllAmount.Name = "OverAllAmount";
+            this.OverAllAmount.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 450);
+            this.Controls.Add(this.EnchantList);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SwapEnchant);
+            this.Controls.Add(this.SaveFile);
+            this.Controls.Add(this.Enchantonitem);
+            this.Controls.Add(this.LoadItem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ItemListBox);
+            this.Controls.Add(this.LoadCharacter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CharacterNames);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Enchantonitem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CharacterNames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button LoadCharacter;
+        private System.Windows.Forms.ListBox ItemListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button LoadItem;
+        private System.Windows.Forms.DataGridView Enchantonitem;
+        private System.Windows.Forms.Button SaveFile;
+        private System.Windows.Forms.Button SwapEnchant;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox EnchantList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Enchant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EnchantAmount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OverAllAmount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,273 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace StoneShardItemEnchantEditor
+{
+    public partial class Form1 : Form
+    {
+        string fileData;
+        JObject rss;
+        List<Enchant> ens;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListOfEnchants();
+            DirectoryInfo ss = new DirectoryInfo("C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\StoneShard\\characters\\");
+
+            DirectoryInfo[] path = ss.GetDirectories();
+            foreach (DirectoryInfo file in path)
+            {
+                CharacterNames.Items.Add(file.Name);
+            }
+        }
+
+        private void LoadCharacter_Click(object sender, EventArgs e)
+        {
+            ItemListBox.Items.Clear();
+            string path = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\StoneShard\\characters\\" + CharacterNames.SelectedItem + "\\save_1\\character.sav";
+            //check for save file file
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("No Save File Exists for this character.");
+                return;
+            }
+            StreamReader sr = new StreamReader(path);
+            
+            string jasontext = sr.ReadLine();
+            fileData = jasontext;
+            //dynamic array = JsonConvert.DeserializeObject(jasontext);
+            rss = JObject.Parse(jasontext);
+            
+            var inventory = (JArray)rss["inventory"];
+            int i = 0;
+            foreach (var stone in inventory)
+            {
+                Console.WriteLine(stone[0].ToString());
+                ItemListBox.Items.Add(i + " " + stone[0].ToString());
+                i++;
+            }
+
+        }
+
+        private void LoadItem_Click(object sender, EventArgs e)
+        {
+
+            refreshEnchant();
+
+            //var inventory = (JObject)rss["inventory"][ItemListBox.SelectedIndex][1]["key"];
+            //foreach (var stone in inventory)
+            //{
+            //    Console.WriteLine(stone.Key);
+
+            //}
+
+        }
+        private void refreshEnchant()
+        {
+            Enchantonitem.Rows.Clear();
+
+            if (rss["inventory"][ItemListBox.SelectedIndex][1]["key"] != null)
+            {
+
+                var inventory = (JObject)rss["inventory"][ItemListBox.SelectedIndex][1];
+                foreach (var stone in inventory)
+                {
+
+                   Console.WriteLine(stone);
+
+                }
+
+                if (rss["inventory"][ItemListBox.SelectedIndex][1]["key"].ToString().Trim() != "")
+                {
+                    string enchant;
+                    string name;
+                    int ammount = 0;
+                    int id = ItemListBox.SelectedIndex;
+                    string overall = "";
+                    JToken texst = rss["inventory"][ItemListBox.SelectedIndex][1]["key"];
+                    enchant = texst.ToString();
+                    if (ens.FirstOrDefault(x => x.devname == enchant) != null)
+                    {
+                        name = ens.Where(x=>x.devname == enchant).Select(x=>x.IGn).Single().ToString();
+                    }
+                    else
+                    {
+                        name = "N/A";
+                    }
+                    if (rss["inventory"][ItemListBox.SelectedIndex][1]["Char0"] != null)
+                    {
+
+                        ammount = int.Parse(rss["inventory"][ItemListBox.SelectedIndex][1]["Char0"].ToString().Split(' ')[1].ToString().Replace("%", "").Replace("+", "").ToString());
+                    }
+
+                    if (rss["inventory"][ItemListBox.SelectedIndex][1][enchant] != null)
+                    {
+                        overall = rss["inventory"][ItemListBox.SelectedIndex][1][enchant].ToString();
+                    }
+
+
+                    Enchantonitem.Rows.Add(id,enchant, name, ammount, overall);
+                    //change list of possible enchants
+                    changeEnchantList(rss["inventory"][ItemListBox.SelectedIndex][1]["Metatype"].ToString());
+                }
+                else
+                {
+                    MessageBox.Show("No Enchant Found");
+                }
+            }
+        }
+
+
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            string path = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\StoneShard\\characters\\" + CharacterNames.SelectedItem  + "\\save_1\\character.sav";
+            try
+            {
+                File.WriteAllText(path, Newtonsoft.Json.JsonConvert.SerializeObject(rss));
+                MessageBox.Show("File Saved");
+            }
+            catch
+            {
+                MessageBox.Show("File Could not be saved over try again later.");
+            }
+        }
+
+        private void swap()
+        {
+            string devnamed;
+            string ConvientName;
+            float amount = 0;
+            string overall = "";
+            string percent = "";
+            JObject test;
+            string type;
+            int id = 0;
+
+            if (Enchantonitem.SelectedRows.Count > 0 && EnchantList.SelectedItems.Count > 0)
+            {
+                DataGridViewRow dr = Enchantonitem.SelectedRows[0];
+                Enchant eee = ens.Where(x => x.IGn == EnchantList.SelectedItem.ToString()).Single();
+                id = int.Parse(dr.Cells[0].Value.ToString());
+                type = eee.type;
+                //type = ens.Select(x => x.type).FirstOrDefault(x => x.name == EnchantList.SelectedItem.ToString()).ToString();
+                //check to see if enchants are capaitable
+                if (rss["inventory"][id][1]["Metatype"].ToString() == type && Enchantonitem.SelectedRows[0].Cells[0].ToString() != "")
+                {
+                    //=======================================================================================================
+                    // remove enchant 
+                    //=======================================================================================================
+                    devnamed = Enchantonitem.SelectedRows[0].Cells[1].Value.ToString();
+                    ConvientName = Enchantonitem.SelectedRows[0].Cells[2].Value.ToString();
+                    amount = float.Parse(Enchantonitem.SelectedRows[0].Cells[3].Value.ToString());
+                    overall = Enchantonitem.SelectedRows[0].Cells[4].Value.ToString();
+
+                    rss["inventory"][id][1]["key"] = "";
+                    if (float.Parse(overall) != amount)
+                    {
+                        rss["inventory"][id][1][devnamed] = float.Parse(overall) - amount;
+                    }
+                    else
+                    {
+                        JObject test1 = (JObject)rss["inventory"][id][1];
+
+                        test1.Property(devnamed).Remove();
+                        //rss["inventory"][ItemListBox.SelectedIndex][1][enchant].Remove();
+
+                    }
+
+                    test = (JObject)rss["inventory"][id][1];
+
+                    test.Property("Char0").Remove();
+                    //=======================================================================================================
+                    //add enchant
+                    //=======================================================================================================
+                    //get info from known enchant list
+                    devnamed = eee.devname;
+                    amount = eee.ammount;
+                    percent = eee.percentage;
+
+                    rss["inventory"][id][1]["key"] = devnamed;
+                    //get item properties
+                    test = (JObject)rss["inventory"][id][1];
+                    //if enchant propertie exists on item already
+                    if (rss["inventory"][id][1][devnamed] != null)
+                    {
+                        rss["inventory"][id][1][devnamed] = float.Parse(rss["inventory"][id][1][devnamed].ToString()) + amount;
+                    }
+                    else
+                    {
+                        test.Property("key").AddAfterSelf(new JProperty(devnamed, amount));
+                    }
+                    //if possitive
+                    if (amount > 0)
+                    {
+                        test.Property("key").AddAfterSelf(new JProperty("Char0", devnamed + " +" + amount + percent));
+                    }
+                    else
+                    {
+                        test.Property("key").AddAfterSelf(new JProperty("Char0", devnamed + " " + amount + percent));
+                    }
+                    //=======================================================================================================
+
+                    refreshEnchant();
+
+                }
+                else
+                {
+                    MessageBox.Show("Item Cannot have that type of enchant please choose another");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select Enchant Row.");
+            }
+            
+            
+        }
+
+        private void ListOfEnchants()
+        {
+            ens = new List<Enchant>();
+            ens.Add(new Enchant() {devname = "EVS",IGn = "Dodge Chance", ammount = 3,type = "Armor" , percentage = "%" });
+            ens.Add(new Enchant() { devname = "CTA", IGn = "Counter Chance", ammount = 5 ,type = "Weapon", percentage = "%" });
+            ens.Add(new Enchant() { devname = "FMB", IGn = "Fumble Chance", ammount = -5, type = "Weapon", percentage = "%" });
+            ens.Add(new Enchant() { devname = "CRT", IGn = "Crit Chance", ammount = 5, type = "Weapon", percentage = "%" });
+            ens.Add(new Enchant() { devname = "PRR", IGn = "Block Chance", ammount = 5, type = "Weapon", percentage = "%" });
+            ens.Add(new Enchant() { devname = "Hit_Chance", IGn = "Accuracy", ammount = 5, type = "Weapon", percentage = "%" });
+        }
+
+        private void changeEnchantList(string typez)
+        {
+            EnchantList.Items.Clear();
+            EnchantList.Items.AddRange(ens.Where(x=>x.type == typez).Select(x => x.IGn).ToArray());
+        }
+
+        private void SwapEnchant_Click(object sender, EventArgs e)
+        {
+            swap();
+        }
+    }
+    class Enchant
+    {
+        public string devname; //name originaly given by devs
+        public string IGn; //in game name
+        public float ammount; 
+        public string type;
+        public string percentage; // % sign if needed at the end
+    }
+}
